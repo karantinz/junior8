b795992e800a935e9074f08d72d5bea88407413d
Zmeegolov <karantinz@mail.ru>
2023-04-03 18:12:57 +0300

    Добавьте файлы проекта.

---
+++ junior8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ваше сообщение: ");
+string userMessage = Console.ReadLine();
+
+Console.Write("Сколько раз это сообщение должно повториться: ");
+int numberOfRepetitions = Convert.ToInt32(Console.ReadLine());
+
+while (numberOfRepetitions-- > 0)
+{
+    Console.WriteLine(userMessage);
+}
